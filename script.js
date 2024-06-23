6431b4a9a036a281e2cbeb99c1af5bca46bce24a
Hebert Lopes <105159975+Fourt14n@users.noreply.github.com>
2024-06-23 11:13:32 -0300

    Finalizing the second screen

--- script.js
+++ script.js
@@ -3,3 +3,41 @@ function changeSecondScreen(){
     document.querySelector(".secondScreen").style.display = "grid";
 }
 
+function eraseBackgrounds(){
+    const malditosBotoes = document.querySelectorAll(".inputsButton")
+    for(i = 0; i < malditosBotoes.length; i++){
+        malditosBotoes[i].style.backgroundColor = ""
+    }
+}
+
+function eraseCheked(){
+    const inputs = document.querySelectorAll(".inputs");
+    for(i = 0; i < inputs.length; i++){
+        inputs[i].removeAttribute("checked");
+    }
+}
+
+const rockDiv = document.querySelector("#rockDiv")
+rockDiv.addEventListener("click", e => {
+    eraseBackgrounds();
+    rockDiv.style.backgroundColor = "rgba(240, 248, 255, 0.14)"
+    eraseCheked()
+    document.querySelector("#rock").setAttribute("checked", true)
+})
+
+const paperDiv = document.querySelector("#paperDiv")
+paperDiv.addEventListener("click",  e => {
+    eraseBackgrounds();
+    paperDiv.style.backgroundColor = "rgba(240, 248, 255, 0.14)"
+    eraseCheked()
+    document.querySelector("#paper").setAttribute("checked", true)
+})
+
+const scissorDiv = document.querySelector("#scissorDiv")
+scissorDiv.addEventListener("click", e => {
+    eraseBackgrounds();
+    scissorDiv.style.backgroundColor = "rgba(240, 248, 255, 0.14)"
+    eraseCheked()
+    document.querySelector("#scissor").setAttribute("checked", true)
+})
+
