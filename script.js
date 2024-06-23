00fa1b12182790f877cea7835f83566438f61454
Hebert Lopes <105159975+Fourt14n@users.noreply.github.com>
2024-06-23 15:58:04 -0300

    Commenting the code

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//The function bellow is called using html to change for the second screen
 function changeSecondScreen(){
     document.querySelector(".firstScreen").style.display = "none";
     document.querySelector(".secondScreen").style.display = "grid";
 }
-
+// The functions bellow are part of the feedback on the choose buttons
 function eraseBackgrounds(){
     const malditosBotoes = document.querySelectorAll(".inputsButton")
     for(i = 0; i < malditosBotoes.length; i++){
@@ -17,6 +18,7 @@ function eraseCheked(){
     }
 }
 
+//Each sequence of code bellow is responsable to make the feedback of the choosing button, using the functions above, this right bellow is for the rock option
 const rockDiv = document.querySelector("#rockDiv")
 rockDiv.addEventListener("click", e => {
     eraseBackgrounds();
@@ -24,7 +26,7 @@ rockDiv.addEventListener("click", e => {
     eraseCheked()
     document.querySelector("#rock").setAttribute("checked", true)
 })
-
+// This right there, for the paper
 const paperDiv = document.querySelector("#paperDiv")
 paperDiv.addEventListener("click",  e => {
     eraseBackgrounds();
@@ -32,7 +34,7 @@ paperDiv.addEventListener("click",  e => {
     eraseCheked()
     document.querySelector("#paper").setAttribute("checked", true)
 })
-
+// And this right here, for the scissors option
 const scissorDiv = document.querySelector("#scissorDiv")
 scissorDiv.addEventListener("click", e => {
     eraseBackgrounds();
